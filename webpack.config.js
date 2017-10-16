edc3a5f61a99fc0cca119867312a90336b33f5c4
renshijie <asdfg@asd.com>
2017-10-16 15:18:50 +0800

    vue练习小项目

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,7 @@ module.exports={
        {
         test: /\.less/,  //打包 .less文件
         loader:'style-loader!css-loader!less-loader'
-      }
-      ,
+      },
        {
         test: /\.(png|jpg|gif|ttf|svg)$/,  //打包 url请求的资源文件
         loader:'url-loader?limit=20000' //limit表示图片的大小为20K是临界值，小于20K的图片均被打包到build.js中去，请求图片就会很快
